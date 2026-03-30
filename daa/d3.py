cfe060c0fe759bb771ee3f54b452837b1df51c45
Gopinath S <kgrjs061@gmail.com>
2026-03-30 09:35:05 +0530

    Update d3.py ph

--- daa/d3.py
+++ daa/d3.py
@@ -11,6 +11,13 @@ def partition(A, low, high):
     return i
 
 
+
+
+
+
+
+
+
 def quickselect(A, low, high, k):
     if low <= high:
         pi = partition(A, low, high)
@@ -31,3 +38,35 @@ target = n - k
 result = quickselect(arr, 0, n - 1, target)
 
 print("kth largest element =", result)
+
+
+
+
+
+//Quick Sort
+
+
+def partition(A, low, high):
+    pivot = A[high]
+    i = low
+
+    for j in range(low, high):
+        if A[j] <= pivot:
+            A[i], A[j] = A[j], A[i]
+            i += 1
+
+    A[i], A[high] = A[high], A[i]
+    return i
+
+
+def quicksort(A, low, high):
+    if low < high:
+        pi = partition(A, low, high)
+        quicksort(A, low, pi - 1)
+        quicksort(A, pi + 1, high)
+
+
+arr = [7, 5, 3, 1, 12, -16, 4, -2]
+quicksort(arr, 0, len(arr) - 1)
+
+print("Sorted array =", arr)
